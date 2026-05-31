31d211c1d8535b73780675bfce4b397671389d3e
Juan Torres <juanm.torresve@ecci.edu.co>
2026-05-30 22:03:34 -0500

    feat: add cart drawer component

---
+++ src/app/features/cart/components/cart-drawer/cart-drawer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-drawer',
+  imports: [],
+  templateUrl: './cart-drawer.component.html',
+  styleUrl: './cart-drawer.component.scss',
+})
+export class CartDrawerComponent {}
