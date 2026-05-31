acdbc86cada16beb2293205fdfaec5ec71153ff0
Juan Torres <juanm.torresve@ecci.edu.co>
2026-05-30 20:46:32 -0500

    fix: update app routes and product card component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./features/catalog/pages/catalog/catalog.component')
+                .then(m => m.CatalogComponent)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
--- src/app/features/catalog/components/product-card/product-card.component.ts
+++ src/app/features/catalog/components/product-card/product-card.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product } from '../../../../core/model';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  standalone: true,
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  product = input.required<Product>();
+  onAddToCart = output<Product>();
+
+  addToCart(event: Event): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.onAddToCart.emit(this.product());
+  }
+}
