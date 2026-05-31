400804a2a4ad2395479c14fd3f446432824e1c7b
Juan Torres <juanm.torresve@ecci.edu.co>
2026-05-30 21:21:13 -0500

    feat: configure lazy loading routes for catalog and product detail, add product catalog page with grid layout

--- src/app/features/catalog/pages/catalog/catalog.component.ts
+++ src/app/features/catalog/pages/catalog/catalog.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CartService, ProductService } from '../../../../core/services';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { Product } from '../../../../core/model';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  standalone: true,
+  imports: [ProductCardComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss',
 })
-export class CatalogComponent {}
+
+export class CatalogComponent implements OnInit{
+  private productService = inject(ProductService);
+  private cartService = inject(CartService);
+
+  products = this.productService.filteredProducts;
+  loading = this.productService.loading;
+  error = this.productService.error;
+  
+  ngOnInit(): void {
+    this.productService.loadProducts();
+    this.productService.loadCategories();
+  }
+
+  onAddToCart(product: Product): void {
+    this.cartService.addToCart(product);
+  }
+}
